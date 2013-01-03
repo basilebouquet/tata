ce95b656638bf9e91834a0278e704de282d98acd
basile bouquet <bas.bouquet@gmail.com>
2013-01-03 10:43:00 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tata::Application.config.secret_token = '1f135433f720573457bfd4d491b92d70701cb504ec17d336588b1b607bf91beed4eebd4bc13a83474926a91418b110c395010f51bc5e9939276ff6701eb9e8bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tata::Application.config.session_store :cookie_store, key: '_tata_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tata::Application.config.session_store :active_record_store
